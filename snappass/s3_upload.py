1ede6334dd48e229b7b96097b292dd8e8aaf5eca
Matt Larsen <matt.larsen@connorgp.com>
2020-10-06 15:55:33 -0600

    static files upload script

--- snappass/s3_upload.py
+++ snappass/s3_upload.py
@@ -6,9 +6,24 @@ env = environ.Env(
 )
 env.read_env()
 
+filepath_headers = {
+    r'.css$': {
+        'Content-Type': 'text/css; charset=utf-8',
+    },
+    r'.js$': {
+        "Content-Type": 'application/javascript; charset=utf-8'
+    }
+}
+
+general_headers = {
+    "Cache-Control": "max-age=86400",
+}
+
 app = Flask(__name__)
 app.config["STATIC_URL"] = env("STATIC_URL", default="static")
 app.config["FLASKS3_BUCKET_NAME"] = env("S3_BUCKET_NAME")
+app.config["FLASKS3_FILEPATH_HEADERS"] = filepath_headers
+app.config["FLASKS3_HEADERS"] = general_headers
 s3 = FlaskS3(app)
 
 create_all(app)
